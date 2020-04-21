c8392ae68a8d9c276e3da7e77c596b1f06476aba
Neil Empey <argus.two.2@gmail.com>
2020-04-21 20:46:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+class Dog
+  @@all = Array.new()
+  attr_accessor :name
+  def initialize(name)
+    self.name = name
+    save()
+  end
+
+  def self.name
+    @name
+  end
+
+  def self.all
+    @@all
+
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.print_all()
+    print @@all
+  end
+
+    def save()
+     
+      @@all.push(Dog.new(self))
+    end
+
+end
+
